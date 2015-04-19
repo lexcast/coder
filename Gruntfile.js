c694230c971eeba153730476b779445d5a230f17
Daniel Alejandro Cast <lexcast13@gmail.com>
2015-04-19 15:14:17 -0600

    configured build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,28 @@ grunt.initConfig({
                 expand: true
             }
         }
-    }
+    },
+    nodewebkit: {
+        options: {
+            platforms: ['linux64'],
+            buildDir: './builds',
+        },
+        src: [
+            './package.json',
+            './LICENSE',
+            './index.html',
+            './resources/*',
+            './css/*',
+            './js/*',
+            './vendor/**/*',
+        ]
+      },
 });
 
 grunt.loadNpmTasks('grunt-bower');
+grunt.loadNpmTasks('grunt-node-webkit-builder');
 
 grunt.registerTask('default', ['bower']);
+grunt.registerTask('build', ['nodewebkit']);
 
 };
